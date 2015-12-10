418b13ed1c5116081c642d994b973ae8bd654072
Pavel Chechetin <paulche@yandex.ru>
2015-12-09 23:14:39 -0600

    Update ubuntu_service_override.pp

--- deployment/puppet/tweaks/manifests/ubuntu_service_override.pp
+++ deployment/puppet/tweaks/manifests/ubuntu_service_override.pp
@@ -4,28 +4,25 @@ define tweaks::ubuntu_service_override (
 ) {
   if $::operatingsystem == 'Ubuntu' {
     $override_file = "/etc/init/${service_name}.override"
-    $file_name     = "create_${service_name}_override"
-    $exec_name     = "remove_${service_name}_override"
-
-    file { $file_name :
-      ensure  => present,
-      path    => $override_file,
-      content => 'manual',
-      mode    => '0644',
-      owner   => 'root',
-      group   => 'root',
-    }
-
+    $exec_create_name     = "create_${service_name}_override"
+    $exec_remove_name     = "remove_${service_name}_override"
+    
+    exec { $exec_create_name :
+      path    => [ '/sbin', '/bin', '/usr/bin', '/usr/sbin' ],
+      command => "echo 'manual' > ${override_file}",
+      unless  => "dpkg -l ${package_name}",
+    } 
+    
     exec { $exec_name :
       path    => [ '/sbin', '/bin', '/usr/bin', '/usr/sbin' ],
       command => "rm -f ${override_file}",
       onlyif  => "test -f ${override_file}",
     }
 
-    File[$file_name] -> Package <| name == $package_name |> -> Exec[$exec_name]
-    File[$file_name] -> Package <| title == $package_name |> -> Exec[$exec_name]
-    File[$file_name] -> Exec[$exec_name]
-    Exec[$exec_name] -> Service <| name == $service_name |>
-    Exec[$exec_name] -> Service <| title == $service_name |>
+    Exec[$exec_create_name] -> Package <| name == $package_name |> -> Exec[$exec_remove_name]
+    Exec[$exec_create_name] -> Package <| title == $package_name |> -> Exec[$exec_remove_name]
+    Exec[$exec_create_name] -> Exec[$exec_remove_name]
+    Exec[$exec_remove_name] -> Service <| name == $service_name |>
+    Exec[$exec_remove_name] -> Service <| title == $service_name |>
   }
 }
